a84de2d7bff8b1a8a9f41021f549449c9d2f4718
Elio Hoyos <elio18david@gmail.com>
2020-03-19 16:40:08 -0500

    tipo camello

---
+++ src/main/java/elio/com/reglasvariable/reglas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elio.com.reglasvariable;
+
+/**
+ *
+ * @author FRANKOYCI
+ */
+public class reglas {
+    public static void main(String[] args) {
+        //variables compatibles en jav(no usar mayuscula en JAVa)
+        //tipo camello
+        var saludarPalabra = "Hola desde Java";
+        System.out.println(saludarPalabra);
+    }
+}
